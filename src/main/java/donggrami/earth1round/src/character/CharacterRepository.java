bc7e7289948fec8ee52ff89d68dc34f4d2c441fc
jrary <jrary@naver.com>
2022-07-11 17:26:27 +0900

    [Fix] Character file directory, annotation, data type

--- src/main/java/donggrami/earth1round/src/character/CharacterRepository.java
+++
@@ -1,9 +0,0 @@
-package donggrami.earth1round.src.character;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CharacterRepository extends JpaRepository<Character, Long> {
-    Character findByCharacterId(String name);
-}
