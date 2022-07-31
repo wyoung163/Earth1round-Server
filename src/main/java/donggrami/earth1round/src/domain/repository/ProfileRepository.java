9a887b8f4305ba3cd8862c0c7dd8c0b2b0adeb99
jrary <jrary@naver.com>
2022-08-01 02:14:15 +0900

    [Fix]Modify name/nickname

--- src/main/java/donggrami/earth1round/src/domain/repository/ProfileRepository.java
+++ src/main/java/donggrami/earth1round/src/domain/repository/ProfileRepository.java
@@ -1,9 +1,14 @@
 package donggrami.earth1round.src.domain.repository;
 
+import donggrami.earth1round.src.domain.entity.Custom;
 import donggrami.earth1round.src.domain.entity.Profile;
+import donggrami.earth1round.src.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ProfileRepository extends JpaRepository<Profile, Long> {
+    Optional<Profile> findByUser(User user);
 }
